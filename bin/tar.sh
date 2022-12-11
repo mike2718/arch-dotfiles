eb3cae86264c4c4ac8a412f83ceac888fb1e8dfa
Mike Akiba <mike2718@gmail.com>
2022-12-12 05:14:47 +0800

    更新

--- bin/tar.sh
+++ bin/tar.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/bash
-rm -f /home/mike/arch-dotfiles.tar.zst
-git archive --verbose --format=tar.zst --prefix=arch-dotfiles/ --output=/home/mike/arch-dotfiles.tar.zst main
+rm -f /home/mike/arch-dotfiles-raspberrypi.tar.zst
+git archive --verbose --format=tar.zst --prefix=arch-dotfiles/ --output=/home/mike/arch-dotfiles-raspberrypi.tar.zst raspberrypi
