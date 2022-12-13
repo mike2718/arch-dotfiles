9e5fc9ce5b882883fac6386983683c239b93506e
Mike Akiba <mike2718@gmail.com>
2022-12-13 19:21:26 +0800

    更新.yashrc

--- bin/tar.sh
+++ bin/tar.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/bash
 rm -f /home/mike/arch-dotfiles-raspberrypi.tar.zst
-git archive --verbose --format=tar.zst --prefix=arch-dotfiles/ --output=/home/mike/arch-dotfiles-raspberrypi.tar.zst raspberrypi
+git archive --verbose --format=tar.zst --prefix=arch-dotfiles-raspberrypi/ --output=/home/mike/arch-dotfiles-raspberrypi.tar.zst raspberrypi
