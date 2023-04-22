1635e22428e2361f151d71935071ac099d7138d4
Mike Akiba <mike2718@gmail.com>
2023-04-23 01:11:29 +0800

    更新

--- bin/mount.sh
+++ bin/mount.sh
@@ -6,6 +6,6 @@
 
 sudo mount -U "85bc3aec-7e1b-47b5-84d7-626ebdb04156" -t ext4 -o defaults /share
 
-sudo systemctl restart qbittorrent-nox@mike.service
+#sudo systemctl restart qbittorrent-nox@mike.service
 sudo systemctl restart syncthing@mike.service
 sudo systemctl restart smb
