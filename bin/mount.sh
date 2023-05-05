fa5391c2e467972f8ca36f033b6b66cd9a28228e
Mike Akiba <mike2718@gmail.com>
2023-05-05 11:04:51 +0800

    更新

--- bin/mount.sh
+++ bin/mount.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 #sudo systemctl stop smb
-#sudo systemctl stop qbittorrent-nox@mike.service
-#sudo systemctl stop lighttpd
 #sudo systemctl stop rtorrent@mike
+#sudo systemctl stop lighttpd
+#sudo systemctl stop syncthing@mike.service
 
 sudo mount -U "85bc3aec-7e1b-47b5-84d7-626ebdb04156" -t ext4 -o defaults /share
 
