987507bddfda23d8fe1152a9f26bf4527f72d799
Mike Akiba <mike2718@gmail.com>
2023-08-16 00:17:18 +0800

    更新

--- bin/mount.sh
+++ bin/mount.sh
@@ -4,6 +4,7 @@ sudo systemctl stop smb
 sudo systemctl stop rtorrent@mike
 sudo systemctl stop lighttpd
 sudo systemctl stop syncthing@mike.service
+killall amuled
 
 sudo mount -U "85bc3aec-7e1b-47b5-84d7-626ebdb04156" -t ext4 -o defaults /share
 
@@ -11,3 +12,5 @@ sudo systemctl restart lighttpd
 sudo systemctl restart rtorrent@mike
 sudo systemctl restart syncthing@mike.service
 sudo systemctl restart smb
+amuled -f
+
