702f792d8b3ef42212bccff4110837ed2d8cb22f
Mike Akiba <mike2718@gmail.com>
2024-05-19 01:16:49 +0800

    更新

--- bin/mount.sh
+++ bin/mount.sh
@@ -8,8 +8,7 @@ killall amuled
 
 sudo mount -U "7d381210-b86f-43c6-aee3-785ea2084200" -t ext4 -o defaults,nosuid,nodev,noatime /share
 
-sudo systemctl restart lighttpd
-sudo systemctl restart rtorrent@mike
+sudo systemctl restart lighttpd && sudo systemctl restart rtorrent@mike
 sudo systemctl restart syncthing@mike.service
 sudo systemctl restart smb
 amuled -f
